c9486ee4a5396e6ce343d14cfa7d944115356cac
John Doe <pablo.gamalero210986@alumnos.info.unlp.edu.ar>
2025-03-17 00:23:31 -0300

    added functions for add friends to the array and clean the box

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let names = [];
 
+function limpiarCaja() {
+    document.querySelector('#amigo').value = ' ';
+    return;
+}
+
+function agregarAmigo() {
+    let name = document.getElementById("amigo").value;
+    if (name != " ") {
+        names.push(name);
+        limpiarCaja();
+        console.log(names);
+    }
+    else {
+        alert("Ingresaste un nombre vacio. Intentalo nuevamente.");
+    }
+    return;
+
+}
